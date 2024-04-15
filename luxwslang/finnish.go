9b23de4873363eb77e847358623f1f4b7cd0dc06
Cyrill Schumacher <cyrill@schumacher.fm>
2024-04-15 21:07:04 +0200

    go mod update, rebase from upstream/main

--- luxwslang/finnish.go
+++ luxwslang/finnish.go
@@ -1,13 +1,16 @@
 package luxwslang
 
-import "regexp"
+import (
+	"strings"
+)
 
 // Finnish language terminology.
 var Finnish = &Terminology{
 	ID:   "fi",
 	Name: "Suomi",
 
-	timestampFormat: "02.01.06 15:04:05",
+	timestampFormat:      "02.01.06 15:04:05",
+	timestampShortFormat: "02.01.06 15:04",
 
 	NavInformation:  "Informaatio",
 	NavTemperatures: "Lämpötilat",
@@ -18,14 +21,28 @@ var Finnish = &Terminology{
 	NavErrorMemory:  "Häiriöloki",
 	NavSwitchOffs:   "Pysähtymistieto",
 
-	NavOpHours:      "Käyttötunnit",
-	HoursImpulsesRe: regexp.MustCompile(`^impulse\s`),
+	NavOpHours: "Käyttötunnit",
+	HoursImpulsesFn: func(s string) bool {
+		return strings.HasPrefix(s, "impulse") || strings.HasPrefix(s, "Impulse")
+	},
 
 	NavSystemStatus:       "Laitetiedot",
 	StatusType:            "Lämpöpumpun tyyppi",
 	StatusSoftwareVersion: "Ohjelmaversio",
 	StatusOperationMode:   "Toimintatila",
+	OperationModeMapping: map[string]float64{
+		// TODO use finnish names
+		// lower case!
+		"off":        OpModeIDOff,
+		"heating":    OpModeIDHeating,
+		"evu":        OpModeIDEVU,
+		"dhw":        OpModeIDDHW,
+		"defrosting": OpModeIDDefrosting,
+	},
 	StatusPowerOutput:     "Kapasiteetti",
+	StatusHeatingCapacity: "Heating capacity", // might be the same as "actual capacity" // TODO use finnish names
+	StatusDefrostDemand:   "Defrost demand",   // TODO use finnish names
+	StatusLastDefrost:     "last defrost",     // TODO use finnish names
 
 	BoolFalse: "Pois",
 	BoolTrue:  "On",
